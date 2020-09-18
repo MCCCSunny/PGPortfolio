1027a3450192fa0efdfd17da0997c4310218ccd7
MCCCSunny <767138291@qq.com>
2020-09-19 01:12:37 +0800

    all

--- result analysis.py
+++ result analysis.py
@@ -6,7 +6,7 @@ from pymongo import MongoClient
 client = MongoClient('localhost', 27017)
 db = client["JointQuant"]
 
-path='F:\\study\\ml\\code\\PGPortfolio\\train_package\\EIIE_CNN\\'
+path='F:\\study\\ml\\code\\PGPortfolio\\train_package\\capsule_EIIE\\'
 dir_ = os.listdir(path)
 
 for onefile in dir_:
