bdda78d38a439b0a75fa6dd13651b1410ba63dd4
MCCCSunny <767138291@qq.com>
2020-09-28 23:28:21 +0800

    all

--- result analysis.py
+++ result analysis.py
@@ -6,7 +6,7 @@ from pymongo import MongoClient
 client = MongoClient('localhost', 27017)
 db = client["JointQuant"]
 
-path='F:\\study\\ml\\code\\PGPortfolio\\train_package\\capsule_EIIE\\'
+path='F:\\study\\ml\\code\\PGPortfolio_loss_trend\\train_package\\capsule_EIIE\\'
 dir_ = os.listdir(path)
 
 for onefile in dir_:
@@ -34,10 +34,15 @@ for onefile in dir_:
             pass
           
 # 平均权重进行交易
+'''
 cash=1000000
 stockList = ["600036", "601166", "601398", "601328", "000001", "600000", "600016"]
 #["600030", "600703", "600050", "600837", "600031"]
-#["600000", "002032", "600498", "000768", "600221"]
+#["600000", "002032", "600498", "000768", "600221"]scipy.optimize._lsap_module, scipy.optimize._differentialevolution, scipy.optimize._shgo, scipy.optimize._shgo_lib, scipy.optimize._shgo_lib.sobol_seq, scipy.optimize._shgo_lib.triangulation, scipy.optimize._dual_annealing, scipy.integrate, scipy.integrate._quadrature, scipy.integrate.odepack, scipy.integrate._odepack, scipy.integrate.quadpack, scipy.integrate._quadpack, scipy.integrate._ode, scipy.integrate.vode, scipy.integrate._dop, scipy.integrate.lsoda, scipy.integrate._bvp, scipy.integrate._ivp, scipy.integrate._ivp.ivp, scipy.integrate._ivp.bdf, scipy.integrate._ivp.common, scipy.integrate._ivp.base, scipy.integrate._ivp.radau, scipy.integrate._ivp.rk, scipy.integrate._ivp.dop853_coefficients, scipy.integrate._ivp.lsoda, scipy.integrate._quad_vec, scipy.misc, scipy.misc.doccer, scipy.misc.common, scipy.stats._constants, scipy.stats._continuous_distns, scipy.interpolate, scipy.interpolate.interpolate, scipy.interpolate.fitpack, scipy.interpolate._fitpack_impl, scipy.interpolate._fitpack, scipy.interpolate.dfitpack, scipy.interpolate._bsplines, scipy.interpolate._bspl, scipy.interpolate.polyint, scipy.interpolate._ppoly, scipy.interpolate.fitpack2, scipy.interpolate.interpnd, scipy.interpolate.rbf, scipy.interpolate._cubic, scipy.interpolate.ndgriddata, scipy.interpolate._pade, scipy.stats._stats, scipy.special.cython_special, scipy.stats._rvs_sampling, scipy.stats._tukeylambda_stats, scipy.stats._ksstats, scipy.stats._discrete_distns, scipy.stats.mstats_basic, scipy.stats._stats_mstats_common, scipy.stats._hypotests, scipy.stats._wilcoxon_data, scipy.stats.morestats, scipy.stats.statlib, scipy.stats.contingency, scipy.stats._binned_statistic, scipy.stats.kde, scipy.stats.mvn, scipy.stats.mstats, scipy.stats.mstats_extras, scipy.stats._multivariate, quantstats.utils, yfinance, yfinance.ticker, requests, requests.exceptions, requests.__version__, requests.utils, requests.certs, requests._internal_utils, requests.compat, requests.cookies, requests.structures, requests.packages, requests.models, requests.hooks, requests.auth, requests.status_codes, requests.api, requests.sessions, requests.adapters, yfinance.base, yfinance.utils, yfinance.shared, yfinance.tickers, yfinance.multi, multitasking, quantstats.plots, quantstats._plotting, quantstats._plotting.wrappers, numpy.lib.recfunctions, numpy.ma.mrecords, scipy.cluster, scipy.cluster.vq, scipy.cluster._vq, scipy.cluster.hierarchy, scipy.cluster._hierarchy, scipy.cluster._optimal_leaf_ordering, quantstats._plotting.core, quantstats.reports, tabulate
+file 31: the profit of 1303296.021209, SR is 3.222049, MDD is -0.078631
+file 75: the profit of 818731.025129, SR is 2.600522, MDD is -0.043673
+
+In [25]: 
 startDate = '2018-12-06'
 endDate = '2019-12-31'
 allProfit = 0
@@ -49,5 +54,5 @@ for one in stockList:
     profit = num_*(lastPrice-startPrice)
     allProfit += profit
 print ("all profit is %f"%allProfit)
-    
+''' 
     
